cfc2cbeeced8f69fc82d340bf458f7a284350f27
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-05-06 14:43:16 +0300

    first commit

--- src/fibseries/FibSeries.java
+++ src/fibseries/FibSeries.java
@@ -1,4 +1,3 @@
-
 package fibseries;
 
 import java.util.Scanner;
@@ -6,19 +5,18 @@ import java.util.Scanner;
 public class FibSeries {
 
     public static void main(String[] args) {
-        int count,num1=0,num2=1;
-        Scanner c=new Scanner(System.in);
+        int count, num1 = 0, num2 = 1;
+        Scanner c = new Scanner(System.in);
         System.out.println("Enter the length of the series");
-        count=c.nextInt();
-        System.out.println("The ter the length of the series\");\n" +
-"        count=c.nextInt();Fibonacci Series is:");
-        for(int i=0;i<=count;i++)
-        {
-            System.out.println(num1+"");
-            int sumOfPrev=num1+num2;
-            num1=num2;
-            num2=sumOfPrev;
+        count = c.nextInt();
+        System.out.println("The length of the series is: " + count);
+        System.out.println("Fibonacci Series are: ");
+        for (int i = 0; i <= count; i++) {
+            System.out.println(num1 + "");
+            int sumOfPrev = num1 + num2;
+            num1 = num2;
+            num2 = sumOfPrev;
         }
     }
-    
+
 }
---
+++ src/fibseries/prac.java
@@ -0,0 +1,15 @@
+package fibseries;
+
+public class prac {
+
+    // converting double to int
+
+    public static void main(String[] args) {
+        double myDouble = 9.78;
+        int myInt = (int) myDouble; // Manual casting: double to int
+
+        System.out.println(myDouble); // Outputs 9.78
+        System.out.println(myInt); // Outputs 9
+    }
+
+}
